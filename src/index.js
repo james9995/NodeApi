5a3c5feab93dea3fa0c58ccb61e355f1b2a15ea2
James Thompson <49037828+james9995@users.noreply.github.com>
2020-02-02 13:47:01 +0000

    build fixed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import express from 'express';
 import { setupDatabase } from './setupDatabase';
 import routes from './routes';
 
+//const fetch = require('node-fetch');
+
+// fetch('http://doxaapi.appspot.com/question/')
+//   .then(res => res.text())
+//   .then(body => console.log(body));
+
 global.database = setupDatabase();
 
 const app = express();
