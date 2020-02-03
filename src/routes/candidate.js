763f24eb60286c8c6f89ecae703aa14cca0c5028
James Thompson <49037828+james9995@users.noreply.github.com>
2020-02-03 05:55:30 +0000

    Change to safe email check, and added fields to post Candidate

--- src/routes/candidate.js
+++ src/routes/candidate.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/emailAddresses', async (req, res) => {
+router.get('/checkEmail/:emailAddress', async (req, res) => {
     const data = global.database.Candidate;
-    const candidates = await data.findAll( {attributes: ['EmailAddress'] });
-    return res.send(candidates);
+    const count = await data.count({ where: { EmailAddress: req.params.emailAddress } });
+    return res.send(count === 0);
 });
 
 router.get('/:emailAddress', async (req, res) => {
@@ -14,14 +14,16 @@ router.get('/:emailAddress', async (req, res) => {
     return res.send(candidate);
 });
 
-router.put('/:candidateData', async (req, res) => {
+router.post('/:candidateData', async (req, res) => {
   const data = global.database.Candidate;
   const response = await data. create({ values: { 
     RecruitmentTestName: req.params.candidateData.RecruitmentTestName,
     EmailAddress: req.params.candidateData.EmailAddress,
+    SessionID: req.params.candidateData.SessionID,
+    ApplicationDetail: req.params.candidateData.ApplicationDetail,
     RegisteredTime: Date.now()
   }});
-  return res.send('Added ' + req.params.candidateData.EmailAddress);
+  return res.send(response);
 });
 
 export default router;
