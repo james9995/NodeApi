e846950293393d20247bc75bbb47665319abbc1c
James Thompson <49037828+james9995@users.noreply.github.com>
2020-02-03 07:00:07 +0000

    Removed extra post method

--- src/routes/candidate.js
+++ src/routes/candidate.js
@@ -28,15 +28,4 @@ router.post('/', async (req, res) => {
   return res.send(response);
 });
 
-UserRouter.route('/create').post(function (req, res) {
-    const user = new User(req.body);
-    user.save()
-      .then(user => {
-        res.json('User added successfully');
-      })
-      .catch(err => {
-        res.status(400).send("unable to save to database");
-      });
-  });
-
 export default router;
